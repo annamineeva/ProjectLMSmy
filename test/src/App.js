9bbae96515debcf3d9c4b74facc4d31675ee3d70
Ardor <zvetok.lolo2@gmail.com>
2023-10-26 21:51:32 +0300

    three commit

---
+++ test/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Stocks from './components/stocks';
+import Main from './components/main';
+import Header from './components/header';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Main></Main>
+    </div>
+  );
+}
+
+export default App;
